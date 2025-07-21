f1a2a230ce8e7f1b2d3c5b540d45c9cbb6c0fdc3
Alexandra Marum <alexandramarum@unknown5e9e6e7a98e6.attlocal.net>
2025-07-20 20:07:58 -0400

    Create gatherly app

---
+++ gatherly/gatherly/gatherlyApp.swift
@@ -0,0 +1,17 @@
+//
+//  gatherlyApp.swift
+//  gatherly
+//
+//  Created by Alexandra Marum on 7/20/25.
+//
+
+import SwiftUI
+
+@main
+struct gatherlyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ gatherly/gatherlyTests/gatherlyTests.swift
@@ -0,0 +1,16 @@
+//
+//  gatherlyTests.swift
+//  gatherlyTests
+//
+//  Created by Alexandra Marum on 7/20/25.
+//
+
+import Testing
+
+struct gatherlyTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
